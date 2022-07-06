e014462af8125d8852d00f167d5fa78a4d53c518
FarooqKollect <farooq.kollect.systems@gmail.com>
2022-07-06 14:22:25 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Nav from './layouts/nav.svelte'
+    import Aside from './layouts/aside.svelte'
+    import Content from './layouts/contents.svelte'
+	export let name;
+</script>
+ <Aside />
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+   
+    <Nav />
+    <Content />
+</main>
+
+
